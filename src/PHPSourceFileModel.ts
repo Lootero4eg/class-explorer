a4678497fb5fb88bb212d481abe7f2bf4b929b8a
Lootero4eg <_papacrash_@mail.ru>
2017-12-13 20:11:41 +0500

    123

--- src/PHPSourceFileModel.ts
+++ src/PHPSourceFileModel.ts
@@ -9,9 +9,9 @@ export class PHPSourceFileModel implements ISourceFileModel{
     }
 
     public getTree(): Branch[]{
-        let phpfile: string = this.editor.document.getText();
-        let classnames: string[] = phpfile.match(/class.*/g);
-        let branch: Branch = new Branch();
+        //let phpfile: string = this.editor.document.getText();
+        //let classnames: string[] = phpfile.match(/class.*/g);
+        /*let branch: Branch = new Branch();
 
         if(classnames != null && classnames.length > 0){
             if(classnames.length >= 2){
@@ -21,19 +21,17 @@ export class PHPSourceFileModel implements ISourceFileModel{
                 branch.EndLine = 0;
                 branch.Icon = 1;  
                 branch.Nodes = [];
-                //this.getChildBranch2(branch)
+                classnames.forEach(element => {
+                    
+                });
+                this.getChildren2(branch, classnames);
             }
             
-        }
-        
-        branch.Name="MyClass";
-        branch.Icon=1;
-        branch.Type = BranchType.Class;
+        }*/                
 
-        let res: Branch[] = [];
+        let res: Branch[] = this.getChildren2(null,null);
         //branch.Nodes = this.getChildren(branch.Nodes[0]);
-
-        res.push(branch);
+        //res.push(branch);
 
         return res;
     }
@@ -42,7 +40,92 @@ export class PHPSourceFileModel implements ISourceFileModel{
         return null;
     }
 
-    private getChildren2(node: Branch, array: string[], btype: BranchType): Branch[]{
-        return null;
+    private getChildren2(node: Branch, itemname: string): Branch[]{
+        let phpfile: string = this.editor.document.getText();
+        let ifaces:Branch[] = [];
+        let classes:Branch[] = [];
+
+        if(node == null){ //--get root
+            node = this.InitNewBranch();            
+            node.Type = BranchType.Namespace;
+            node.Name = "Namespace";            
+        }
+
+        switch(node.Type){
+            case BranchType.Namespace:
+                let namespaces: string[] = phpfile.match(/namespace.*/g);
+                if(namespaces != null && namespaces.length > 0){
+                    for(var i = 0; i < namespaces.length; i++) {
+                        let namespace: string = namespaces[i].replace(/^.*namespace (.*?);/,"$1");
+                        if(namespace != namespaces[i]){
+                            node.Name = namespace;   
+                            break;                         
+                        }                   
+                    } 
+                }                
+                let nextBranch: Branch = node;
+                if(node.Name != "Namespace"){                    
+                    nextBranch = this.InitNewBranch();
+                }
+                
+                nextBranch.Name = "Interfaces";
+                nextBranch.Type = BranchType.Interfaces;                                                
+                ifaces = this.getChildren2(nextBranch, null);
+                
+                if(ifaces != [] && ifaces[0].Nodes.length > 0)
+                    node.Nodes.concat(ifaces);
+
+                /*nextBranch = node;
+                if(node.Name != "Namespace"){                    
+                    nextBranch = this.InitNewBranch();
+                }*/
+                nextBranch.Name = "Classes";
+                nextBranch.Type = BranchType.Classes;                                                
+                classes = this.getChildren2(nextBranch, null);
+
+                if(classes != [] && classes[0].Nodes.length > 0)
+                node.Nodes.concat(classes);
+
+                break;
+
+            case BranchType.Interfaces:
+                break;
+
+            case BranchType.Classes:
+                let classnames: string[] = phpfile.match(/class .*/g);
+                if(classnames != null && classnames.length > 0){
+                    for(var i = 0; i < classnames.length; i++) {
+                        let nextBranch: Branch = this.InitNewBranch();
+                        nextBranch.Name = "Class";
+                        nextBranch.Type = BranchType.Class;
+                        classes.concat(this.getChildren2(nextBranch, classnames[i]));
+                    }                    
+                }
+                break;
+
+            case BranchType.Class:
+                let classname: string = itemname.replace(/^.*class (.*?)/,"$1");
+                classname = classname.replace(/^(.*?) extends (.*).*$/,'$1(base: $2)');
+                break;
+        }
+
+        let res: Branch[] = [];
+        /*if(ifaces != [] && ifaces[0].Nodes.length > 0)
+            res.concat(ifaces);*/
+        if(node != null)
+            res.push(node);
+        return res;
+    } 
+    
+    private InitNewBranch(): Branch{
+        let branch: Branch = new Branch();
+        branch.Type = BranchType.None;
+        branch.Name = "";
+        branch.StartLine = 0;
+        branch.EndLine = 0;
+        branch.Icon = 0;
+        branch.Nodes = [];
+
+        return branch;
     }
 }
--- src/common.ts
+++ src/common.ts
@@ -14,6 +14,7 @@ export interface ISourceFileModel {
 }
 
 export enum BranchType {
+    None,
     Namespace,
     Classes,
     Class,
